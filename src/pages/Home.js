bfb6cda42f46bb7ed616cd171cb46d169fc6087b
charlescampista <charlescampista@gmail.com>
2023-02-28 11:29:35 -0300

    2-Part

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,86 @@
 import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View, FlatList } from 'react-native'
 
 export default function Home() {
   return (
-    <View style={styles.header}>
+    <View style={styles.container}>
+      <View style={styles.header}>
         <View>
-          <Text style={styles.text}> Your name </Text>
+          <Text style={styles.text}> Hi {"<Your Name>"}!! </Text>
           <Text style={styles.text}> Good Morning! </Text>
-
         </View>
         <Image 
         style={styles.image}
         source={{uri: "https://picsum.photos/300/300"}}/>
+       
+      </View>
+
+      <View style={styles.tagContainer}>
+      <FlatList
+          data={[
+            "smartphones",
+            "laptops",
+            "fragrances",
+            "skincare",
+            "groceries",
+            "home-decoration",
+            "furniture",
+            "tops",
+            "womens-dresses",
+            "womens-shoes",
+            "mens-shirts",
+            "mens-shoes",
+            "mens-watches",
+            "womens-watches",
+            "womens-bags",
+            "womens-jewellery",
+            "sunglasses",
+            "automotive",
+            "motorcycle",
+            "lighting"
+            ]}
+          renderItem={({item}) => 
+          <View style={styles.tag}>
+            <Text>{item}</Text>
+          </View>}
+        />
+      </View>
+    
     </View>
   )
 }
 
 
 const styles = StyleSheet.create({
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginHorizontal: 10,
-        marginVertical: 20
-    },
-    
-    image: {
-        width: 70,
-        height: 70,
-        borderRadius: 35
-    },
-    text: {
-        fontFamily: 'Inter_900Black',
-    }
+
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    marginVertical: 37
+  },
+  tag: {
+    width: 60,
+    height: 32,
+    borderRadius: 12
+  },
+
+  tagContainer: {
+    flexDirection: 'row'
+  },
+
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35
+  },
+  text: {
+    fontFamily: 'Inter_900Black',
+  }
 });
